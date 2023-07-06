3b8d7210f80e0316d46cb74330fb9c83c4ef3cb5
Matheus Mattos Avelino <matheusma2510@hotmail.com>
2023-07-06 00:38:29 +0000

    temp commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
-import React from 'react'
+'use client'
+
+import { signIn, useSession } from 'next-auth/react'
+import Image from 'next/image'
 
 export default function Home() {
-  return <div>Home</div>
+  const { data } = useSession()
+
+  return (
+    <div>
+      <button onClick={() => signIn()}>Login</button>
+      <h1>{data?.user?.name}</h1>
+      <Image src={data?.user?.image ?? ''} alt="Imagem do Usuário" />
+    </div>
+  )
 }
